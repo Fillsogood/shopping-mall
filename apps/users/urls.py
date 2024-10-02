ee8c6a37e2ffa700f524e7b0d2dd3d81af4fda54
Fillsogood <jdd05273@naver.com>
2024-10-02 18:31:39 +0900

    chore:리드미 작성

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path("signup/", views.SignupViewSet.as_view({"post": "create"}), name="signup"),
     path("login/", views.JWTLoginView.as_view(), name="login"),
-     path("logout/", views.JWTLogoutView.as_view({"post": "post"}), name="logout"),
+    path("logout/", views.JWTLogoutView.as_view({"post": "post"}), name="logout"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", views.JWTRefreshView.as_view({"post": "post"}), name="token_refresh"),
     path("info/", views.UserDetailViewSet.as_view({"get": "retrieve"}), name="user_info"),
